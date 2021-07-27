0547c1b96c69fd3f3506e33d611e649608c6da5a
cmabris <carlosmanuel.abrisqueta@murciaeduca.es>
2021-07-27 19:52:25 +0200

    Fin vídeo 11

--- database/migrations/2021_07_24_173521_create_products_table.php
+++ database/migrations/2021_07_24_173521_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -29,7 +30,7 @@ class CreateProductsTable extends Migration
 
             $table->integer('quantity')->nullable();
 
-            //$table->enum('status', [Product::BORRADOR, Product::PUBLICADO])->default(Product::BORRADOR);
+            $table->enum('status', [Product::BORRADOR, Product::PUBLICADO])->default(Product::BORRADOR);
 
             $table->timestamps();
         });
