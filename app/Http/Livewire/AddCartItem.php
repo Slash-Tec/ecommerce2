dcfeb858d273475005ff79f11aba1c6feb862ebe
cmabris <carlosmanuel.abrisqueta@murciaeduca.es>
2021-08-03 01:44:39 +0200

    Fin vídeo 47

---
+++ app/Http/Livewire/AddCartItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddCartItem extends Component
+{
+    public $qty = 1;
+
+    public function decrement()
+    {
+        $this->qty--;
+    }
+
+    public function increment()
+    {
+        $this->qty++;
+    }
+
+    public function render()
+    {
+        return view('livewire.add-cart-item');
+    }
+}
