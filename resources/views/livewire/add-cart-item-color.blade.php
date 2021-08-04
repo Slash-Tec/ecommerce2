f6989abf39b3a53549d8e2e4908e958d6b182cb2
cmabris <carlosmanuel.abrisqueta@murciaeduca.es>
2021-08-04 18:51:48 +0200

    Fin vídeo 49

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <p class="text-xl text-gray-700">Color:</p>
+
+    <select class="form-control w-full">
+        <option value="" selected disabled>Seleccionar un color</option>
+        @foreach ($colors as $color)
+            <option value="{{$color->id}}">{{ $color->name }}</option>
+        @endforeach
+    </select>
 </div>
