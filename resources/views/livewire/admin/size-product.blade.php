46c7dc093226234c48e2b0bc2523d8bb1dd53825
cmabris <carlosmanuel.abrisqueta@murciaeduca.es>
2021-08-20 20:42:43 +0200

    Fin vídeo 142

--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -1,3 +1,46 @@
 <div>
-    Necesita especificar una talla y un color
+    <div class="bg-white shadow-lg rounded-lg p-6 mt-12">
+        <div>
+            <x-jet-label>
+                Cantidad
+            </x-jet-label>
+            <x-jet-input
+                wire:model="name"
+                type="text"
+                placeholder="Introduzca una talla"
+                class="w-full"/>
+            <x-jet-input-error for="name" />
+        </div>
+
+        <div class="flex justify-end items-center mt-4">
+            <x-jet-button wire:click="save" wire:loading.attr="disabled" wire:target="save">
+                Agregar
+            </x-jet-button>
+        </div>
+    </div>
+
+    <ul class="mt-12 space-y-4">
+        @foreach ($sizes as $size)
+            <li class="bg-white shadow-lg rounded-lg p-6" wire:key="size-{{ $size->id }}">
+                <div class="flex items-center">
+                    <span class="text-xl font-medium">{{ $size->name }}</span>
+
+                    {{--<div class="ml-auto">
+
+                        <x-jet-button wire:click="edit({{ $size->id }})" wire:loading.attr="disabled"
+                                      wire:target="edit({{ $size->id }})">
+                            <i class="fas fa-edit"></i>
+                        </x-jet-button>
+
+                        <x-jet-danger-button wire:click="$emit('deleteSize', {{ $size->id }})">
+                            <i class="fas fa-trash"></i>
+                        </x-jet-danger-button>
+
+                    </div>--}}
+                </div>
+
+{{--                @livewire('admin.color-size', ['size' => $size], key('color-size-' . $size->id))--}}
+            </li>
+        @endforeach
+    </ul>
 </div>
